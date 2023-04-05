4aafec151c25b8b861897f86689703f2148cd6c8
Tommy McDonagh <tommymcdonagh@kubrickgroup.com>
2023-04-05 11:59:53 +0100

    Adding dataframe containing JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header('Fruityvice Fruit Advice')
 import requests
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
 streamlit.text(fruityvice_response.json())
+
+# write the json into a pandas dataframe 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display said dataframe
+streamlit.dataframe(fruityvice_normalized)
