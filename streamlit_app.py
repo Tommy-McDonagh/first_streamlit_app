5db942a0c8b210cc81d118e0d206eba0e8a8a896
Tommy McDonagh <tommymcdonagh@kubrickgroup.com>
2023-04-05 12:02:27 +0100

    Adding a choince of fruit to send to api for response and appending request url

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,12 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
 
+# Adding a choince of fruit to send to api for response
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
 
 
 # write the json into a pandas dataframe 
