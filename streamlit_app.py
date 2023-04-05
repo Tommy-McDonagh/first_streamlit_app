77fd54edcc6867ceb1ef848f46545fc34325dec9
Tommy McDonagh <tommymcdonagh@kubrickgroup.com>
2023-04-05 17:45:21 +0100

    try - except form for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,20 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice')
-
+try:
 # Adding a choice of fruit to send to api for response
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error('Please select a fruit to get information')
+    else:
+        fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
+        # write the json into a pandas dataframe 
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # display said dataframe
+        streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/'+fruit_choice)
-
-
-# write the json into a pandas dataframe 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display said dataframe
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
